b6dfd04d196a5a868b7045d5e1769ccc1e13b5ed
Shreya Shree S <92005528+shreyashree00015@users.noreply.github.com>
2024-05-19 15:56:45 +0530

    Time: 36 ms (69.22%), Space: 16.7 MB (44.01%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,17 +3,26 @@ class Solution:
         if not arr:
             return ''
         
-        prefix = ''
-        first_str = arr[0]
+        # prefix = ''
+#         first_str = arr[0]
         
-        for i in range(len(first_str)):
-            current_char = first_str[i]
-            for other_str in arr:
-                if i >= len(other_str) or other_str[i] != current_char:
-                    return prefix
-            prefix += current_char
+#         for i in range(len(first_str)):
+#             current_char = first_str[i]
+#             for other_str in arr:
+#                 if i >= len(other_str) or other_str[i] != current_char:
+#                     return prefix
+#             prefix += current_char
         
-        return prefix
+#         return prefix
+        
+        s = ''
+        for i in range(len(arr[0])):
+            c = arr[0][i]
+            for j in arr:
+                if i>=len(j) or j[i] != c :
+                    return s
+            s += c
+        return s
                     
                     
                 
