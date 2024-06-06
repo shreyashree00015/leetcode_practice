09c23720fe074b6e4c743fb5dd736e397c8074c4
Shreya Shree S <92005528+shreyashree00015@users.noreply.github.com>
2024-06-06 23:18:03 +0530

    Time: 77 ms (96.20%), Space: 19.6 MB (83.62%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,13 +3,10 @@ from typing import List
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Dictionary to map sorted tuple of characters to list of anagrams
         anagrams = defaultdict(list)
         
         for s in strs:
-            # Sort the string and use it as a key
-            key = tuple(sorted(s))
+            key = ''.join(sorted(s))  # Sort the string and join back to form the key
             anagrams[key].append(s)
         
-        # Return the values of the dictionary which are the grouped anagrams
         return list(anagrams.values())
